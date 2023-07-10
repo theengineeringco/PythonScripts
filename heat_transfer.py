c47aa71dbebde3eaacf2948efa885663c64f809f
Neil Cameron <neil@flowengineering.com>
2023-07-10 16:37:59 +0100

    feat: add heat_transfer and fuel_pump for demo

---
+++ heat_transfer.py
@@ -0,0 +1,47 @@
+"""
+in: flow_velocity 5
+out: total_heat_transfer
+out: injector_plate_dia
+out: injector_ports
+"""
+
+import math
+
+# Constants
+g = 9.81  # m/s
+rho = 1.225  # kg/m3
+
+# Variables
+# flow_velocity = 5  # m/s
+rpm = 10
+power = 8000  # W
+
+# Data
+speed_list = [6.8, 6.0, 7.2, 7.3, 8.2, 9.0]
+width_list = [0.8, 1.0, 1.5, 1.3, 1.2, 1.0]
+
+
+# Functions
+def forced_convection(speed_list, width_list):
+    nussult_no = 0.71  # Constant
+    re_no = [speed_list[i] * width_list[i] / nussult_no for i in range(5)]
+    return re_no
+
+
+# Main calculation
+re_no = forced_convection(speed_list, width_list)
+
+heat_transfer = []
+for i in range(5):
+    heat_transfer.append(2 * re_no[i] * rho * flow_velocity)
+
+
+total_heat_transfer = sum(heat_transfer)
+
+heat_transfer_density = 0.06  # kW/mm2
+injector_plate_area = total_heat_transfer / heat_transfer_density
+injectior_plate_dia = math.ceil(math.sqrt(injector_plate_area) * 2 / math.pi)
+port_spacing = 5  # number per mm
+injector_ports = math.ceil((injectior_plate_dia / port_spacing) * 6)
+
+print(total_heat_transfer)
---
+++ rocket_calcs.py
@@ -0,0 +1,61 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set parameters.
+Ms = 50.0  # weight of rocket shell (kg)
+g = 9.81  # acceleration due to gravity (m.s^{-2})
+rho = 1.091  # Average air density (kg.m^{-3})
+r = 0.5  # radius (m)
+A = math.pi * r**2  # Maximum cross sectional area of the rocket (m^2)
+ve = 325  # exhaust speed (m.s^{-1})
+CD = 0.15  # drag coefficient
+
+
+def rhs_rocket(u, g, ve, A, CD, rho, Mp_dot, Ms):
+    h, v, Mp = u
+    rhs = np.array(
+        [
+            v,
+            -g + Mp_dot * ve / (Ms + Mp) - 0.5 * rho * v**2 * A * CD / (Ms + Mp),
+            -Mp_dot,
+        ]
+    )
+    return rhs
+
+
+# Set initial conditions.
+Mpo = 100.0  # initial weight of the rocket propellant (kg)
+Mp_dot = 20.0  # burning rate at t=0
+v0 = 0.0  # intial velocity
+h0 = 0.0  # initial height
+
+
+def euler_step(u, f, dt, *args):
+    u_new = u + dt * f(u, *args)
+    return u_new
+
+
+T = 15.7  # length of the time interval
+dt = 0.1  # time-step size
+N = int(T / dt) + 1  # number of time steps
+t = np.linspace(0.0, T, num=N)
+
+# Create array to store the solution at each time step.
+u = np.empty((N, 3))
+
+# Set the initial conditions.
+u[0] = np.array([h0, v0, Mpo])
+
+# Time integration using Euler's method.
+for n in range(N - 1):
+    if n > 49:
+        Mp_dot = 0.0  # Burning rate becomes zero after 5 seconds
+    u[n + 1] = euler_step(u[n], rhs_rocket, dt, g, ve, A, CD, rho, Mp_dot, Ms)
+
+u_max = u.max()
+
+plt.figure(figsize=(6.0, 6.0))
+plt.grid()
+plt.plot(t, u[:, 0])
+plt.show()
